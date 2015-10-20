ba28a5c47fcc385a969d9e1b4e318d855e7d9847
Gertjan Roke <gertjan_roke90@hotmail.com>
2015-10-20 11:08:56 +0200

    Updated a view file

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -3,7 +3,7 @@
         @if (Session::has('flash_notification.overlay'))
             @include('notification::modal', ['modalClass' => 'flash-modal', 'title' => Session::get('flash_notification.title'), 'body' => Session::get('flash_notification.message')])
         @else
-        <div class="alert alert-{{ Session::get('flash_notification.level') }}{{ Session::get('flash_notification.important') }}">
+        <div class="alert alert-{{ Session::get('flash_notification.level') }} {{ Session::get('flash_notification.important') }}">
             <div class="row">
                 <div class="col-md-12">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
