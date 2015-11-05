34114f38b4302f23a4179b006f4b268e6f725bf8
Gertjan Roke <gertjan_roke90@hotmail.com>
2015-11-05 19:26:50 +0100

    Removed some html that wasn't needed

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -4,31 +4,27 @@
             @include('notification::modal', ['modalClass' => 'flash-modal', 'title' => Session::get('flash_notification.title'), 'body' => Session::get('flash_notification.message')])
         @else
         <div class="alert alert-{{ Session::get('flash_notification.level') }} {{ Session::get('flash_notification.important') }}">
-            <div class="row">
-                <div class="col-md-12">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    @if(Session::has('flash_notification.title'))
-                        <p class="alert-title">{{ Session::get('flash_notification.title') }}</p>
-                        <hr>
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            @if(Session::has('flash_notification.title'))
+                <p class="alert-title">{{ Session::get('flash_notification.title') }}</p>
+                <hr>
+            @endif
+            @if(is_array(Session::get('flash_notification.message')))
+                @foreach(Session::get('flash_notification.message') as $title => $message)
+                    @if(is_array($message))
+                        <p class="alert-title with-list">{{ $title }}</p>
+                        <ul>
+                            @foreach($message as $text)
+                                <li>{{ $text }}</li>
+                            @endforeach
+                        </ul>
+                    @else    
+                        <p class="alert-message single">{{ $message }}</p>
                     @endif
-                    @if(is_array(Session::get('flash_notification.message')))
-                        @foreach(Session::get('flash_notification.message') as $title => $message)
-                            @if(is_array($message))
-                                <p class="alert-title with-list">{{ $title }}</p>
-                                <ul>
-                                    @foreach($message as $text)
-                                        <li>{{ $text }}</li>
-                                    @endforeach
-                                </ul>
-                            @else    
-                                <p class="alert-message single">{{ $message }}</p>
-                            @endif
-                        @endforeach
-                    @else
-                        {{ Session::get('flash_notification.message') }}
-                    @endif
-                </div>
-            </div>
+                @endforeach
+            @else
+                {{ Session::get('flash_notification.message') }}
+            @endif
         </div>
         @endif
     @endif
